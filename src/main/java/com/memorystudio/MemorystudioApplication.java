ebd7d2fa6f0f92c2d3b39dd0947854a269cb291d
Hyeong Jun Noh <munhwas1140@naver.com>
2023-12-26 15:50:16 +0900

    init

---
+++ src/main/java/com/memorystudio/MemorystudioApplication.java
@@ -0,0 +1,13 @@
+package com.memorystudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemorystudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemorystudioApplication.class, args);
+	}
+
+}
